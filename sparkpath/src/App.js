ec679e495f982035086925d228aa41ce052fc9bb
Sahil Gokhale <sahil.gokhale2279@gmail.com>
2025-07-19 02:21:20 -0400

    signup/in

--- sparkpath/src/App.js
+++ sparkpath/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/navbar';
 import Chatbot from './components/chatbot';
+import SignUpIn from './components/sign-up-in';
+import { useState } from 'react';
 
 function App() {
+  const [signIn, setSignIn] = useState(false);
   return (
     <div className="App">
       <Navbar />
+      <SignUpIn signIn={signIn} setSignIn={setSignIn}/>
       <Chatbot />
     </div>
   );
